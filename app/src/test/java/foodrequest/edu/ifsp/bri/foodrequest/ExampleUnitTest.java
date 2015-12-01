893a67dceeb26d8eacbaaa4cb61bca54e1c40f6e
Diego5529 <diego.6.souza@gmail.com>
2015-12-01 21:40:05 -0200

    Initial commit App

---
+++ app/src/test/java/foodrequest/edu/ifsp/bri/foodrequest/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package foodrequest.edu.ifsp.bri.foodrequest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
